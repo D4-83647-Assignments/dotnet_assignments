7ace2ed1846f52bda34e8b0a602dc9cae3464603
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-05-15 16:15:04 +0530

    Assignment 1, Question 3

---
+++ assignment1/assignment1/question3/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace question3
+{
+    internal class Program
+    {
+
+        public int menu()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            int a, b, result, choice;
+            
+
+            do
+            {
+
+                Console.WriteLine("Enter First Number: ");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number: ");
+                b = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("0. EXIT");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Substraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("Enter choice :");
+                choice = int.Parse(Console.ReadLine());
+
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("EXITING...!");
+                        break;
+
+                    case 1:
+                        result = a + b;
+                        Console.WriteLine("Addition");
+                        Console.WriteLine(a + " + " + b + " = " + result);
+
+                        break;
+
+                    case 2:
+                        result = a - b;
+                        Console.WriteLine("Subtraction");
+                        Console.WriteLine(a + " - " + b + " = " + result);
+
+                        break;
+
+                    case 3:
+                        result = a * b;
+                        Console.WriteLine("Multiplication");
+                        Console.WriteLine(a + " * " + b + " = " + result);
+
+                        break;
+
+                    case 4:
+                        result = a / b;
+                        Console.WriteLine("Division");
+                        Console.WriteLine(a + " / " + b + " = " + result);
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice!!!");
+                        break;
+                }
+            }while (choice != 0);   
+
+        }
+    }
+}
