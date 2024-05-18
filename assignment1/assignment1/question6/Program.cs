4ad0923fb5996c34e3ecd589c5bbba3f1a73d081
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-05-18 15:12:04 +0530

    Question 6

---
+++ assignment1/assignment1/question6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace question6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d1 = new Date();
+            Date d2 = new Date();
+            d1.AcceptDate();
+            d2.AcceptDate();
+            int result = d1 - d2;
+            Console.WriteLine(result);
+            //date.PrintDate();
+            Console.WriteLine("Is Date1 valid: " + d1.IsValid());
+            Console.WriteLine("Is Date2 valid: " + d2.IsValid());
+            //Console.WriteLine(date.ToString());
+        }
+    }
+}
