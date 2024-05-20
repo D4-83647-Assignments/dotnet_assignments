a2143854bf58af475b333b9bd858f65fde2b3528
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-05-20 18:23:12 +0530

    Question 8

---
+++ assignment1/assignment1/question8/Employee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using question7;
+
+namespace question8
+{
+    internal class Employee : Person
+    {
+        private int _id;
+        private double _salary;
+        private string _designation;
+        private DepartmentType _dept;
+        private static int idGenerator = 1001;
+
+        public Employee()
+        {
+            Id = idGenerator++;
+        }
+
+        public Employee(double salary, string designation, DepartmentType dept)
+        {
+            Id = idGenerator++;
+            Salary = salary;
+            Designation = designation;
+            Dept = dept;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public double Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public DepartmentType Dept
+        {
+            get { return _dept; }
+            set { _dept = value; }
+        }
+
+        public string Designation
+        {
+            get { return _designation; }
+            set { _designation = value; }
+        }
+
+        public override void Accept()
+        {
+            base.Accept();
+            Console.Write("Salary: ");
+            Salary = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Designation: ");
+            Designation = Console.ReadLine();
+            Console.WriteLine("Department: ");
+            Console.WriteLine("1. Marketing, 2. Sales, 3. Store");
+            Dept = (DepartmentType)Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(base.ToString());
+            Console.WriteLine(ToString());
+            
+        }
+
+        public override string ToString()
+        {
+            return "Id: "+Id+" Salary: " + Salary + " Designation: " + Designation + " Department: " + Dept;
+        }
+
+    }
+
+    enum DepartmentType
+    {
+        Marketing = 1,
+        Sales = 2,
+        Store = 3
+    }
+}
---
+++ assignment1/assignment1/question8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace question8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.Accept();
+            employee.Print();
+        }
+    }
+}
