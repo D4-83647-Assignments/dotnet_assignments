489a46ac91038f239ba96ea0b0b5804658aaee3b
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-05-21 18:22:35 +0530

    Question 9

---
+++ assignment1/assignment1/question9/Program.cs
@@ -0,0 +1,26 @@
+﻿using question7;
+using question8;
+
+namespace question9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Manager m = new Manager();
+
+            //m.Accept();
+            //m.Print();
+
+            //Employee employee = new Employee();
+            //employee.Accept();
+            //employee.Print();
+
+            Person person = new Manager();
+            Manager m = (Manager)person;
+
+            m.Accept();
+            m.Print();
+        }
+    }
+}
