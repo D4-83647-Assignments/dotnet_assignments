6da75a765a4a449ee83c7c6b989b501bc07d14fd
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-05-25 14:31:17 +0530

    Question 14

---
+++ assignment1/assignment1/question14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using question12;
+using question8;
+
+namespace question14
+{
+    internal class Program
+    {
+        public static int Menu(int choice)
+        {
+            Console.WriteLine("0. EXIT");
+            Console.WriteLine("1. Add Employee to Company");
+            Console.WriteLine("2. Remove Employee from Company");
+            Console.WriteLine("3. Find Employee by Id");
+            Console.WriteLine("4. Display Company Info");
+            Console.WriteLine("5. Display All Employee");
+            return choice;
+        }
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+
+        }
+
+        
+    }
+}
